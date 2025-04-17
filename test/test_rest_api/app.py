ecd5db658d6d5cc0c5bba73a7117fdf9fa7201c7
Kyle M McGrath <kyle@abelprivacy.com>
2025-04-17 08:31:26 -0400

    wip

--- test/test_rest_api/app.py
+++ test/test_rest_api/app.py
@@ -3,6 +3,7 @@
 #sys.path.insert(0, '/Users/kylemcgrath/Documents/GitHub/nginx-with-wolfssl/venv/lib/python3.13/site-packages/')
 
 from flask import Flask
+import ssl
 from db import db
 
 
@@ -18,5 +19,9 @@ def get_request():
 @app.route("/request/", methods=['POST'])
 def post_request():
     return "POST /requests/"
+
+ssl_context = ssl.create_default_ssl_context(ssl.Purpose.CLIENT_AUTH)
+ssl_context.load_cert_chain(certfile="../certs/cert.pem", keyfile="../certs/key.pem")
+ssl_context.set_ciphers()
 
 app.run(host="127.0.0.1", port="8080", ssl_context=("../certs/cert.pem", "../certs/key.pem"))
