bfea5fb6dbd3c55f01e6412effadfdbc10531a9a
Kyle M McGrath <kyle@abelprivacy.com>
2025-04-20 18:02:54 -0400

    Fix eslint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,23 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  { ignores: ["**/dist/**", "**/node_modules/**", "venv"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node,
+    },
+  },
+  {
+    ignores: [
+      "**/build/**",           // compiled native addon binaries
+      "**/dist/**",            // JS output folder
+      "**/node_modules/**",
+      "**/venv/**",
+      "**/*.cpp",              // ignore native C++ files
+      "**/*.h",                // headers
+    ],
+  },
 ]);
