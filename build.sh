86644455448547bf09df05dd3789945db0c7c8a3
Kyle M McGrath <kyle@abelprivacy.com>
2025-04-21 08:13:13 -0400

    Start prototyping post()

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@
 rm -rf build_bak
 mkdir build_bak
 
-cp *.o ./build_bak || :
-cp *.so ./build_bak || :
-cp *.a ./build_bak || :
-cp *.dylib ./build_bak || :
+cp ./*.o ./build_bak || :
+cp ./*.so ./build_bak || :
+cp ./*.a ./build_bak || :
+cp ./*.dylib ./build_bak || :
 
 cp -r build ./build_bak/build || :
 
-rm -f *.o
-rm -f *.so
-rm -f *.a
-rm -f *.dylib
+rm -f ./*.o
+rm -f ./*.so
+rm -f ./*.a
+rm -f ./*.dylib
 
 rm -f metadata.db
 
@@ -26,51 +26,41 @@ tar -xzf build-x86_64.tar.gz
 tar -xzf build-osx-arm64.tar.gz
 
 g++ --std=c++20 ./util/reverse-https-proxy.cpp -I ~/homebrew/include/ \
-    -I build_x86_64/include \
-    -L build_x86_64/lib \
-    -I build-osx-arm64/wolfssl \
-    -L build-osx-arm64/lib -lwolfssl -o ./util/reverse-https-proxy
+	-I build_x86_64/include \
+	-L build_x86_64/lib \
+	-I build-osx-arm64/wolfssl \
+	-L build-osx-arm64/lib -lwolfssl -o ./util/reverse-https-proxy
 
-g++ -std=c++17 -lwolfssl -lsqlite3 -o secure_http_client -c xios.cpp \
-    -I build_x86_64/include \
-    -L build_x86_64/lib \
-    -I build-osx-arm64/wolfssl \
-    -L build-osx-arm64/lib \
-    -L ~/homebrew/Cellar/sqlite/3.49.1/lib \
-    -I ~/homebrew/Cellar/sqlite/3.49.1/include \
-    -o xios.o
+g++ -std=c++17 -lwolfssl -lsqlite3 -o secure_http_client -c src/xios.cpp \
+	-I build_x86_64/include \
+	-L build_x86_64/lib \
+	-I build-osx-arm64/wolfssl \
+	-L build-osx-arm64/lib \
+	-L ~/homebrew/Cellar/sqlite/3.49.1/lib \
+	-I ~/homebrew/Cellar/sqlite/3.49.1/include \
+	-o xios.o
 
 g++ -std=c++17 ./test/test.cpp ./test/test_parseURL.cpp \
-    ./test/test_get.cpp\
-    xios.o \
-    -I build_x86_64/include \
-    -L build_x86_64/lib \
-    -I build-osx-arm64/wolfssl \
-    -L build-osx-arm64/lib \
-    -L ~/homebrew/Cellar/sqlite/3.49.1/lib \
-    -I ~/homebrew/Cellar/sqlite/3.49.1/include \
-    -lwolfssl -lsqlite3 \
-    -o run_tests
+	./test/test_get.cpp xios.o \
+	-I build_x86_64/include \
+	-L build_x86_64/lib \
+	-I build-osx-arm64/wolfssl \
+	-L build-osx-arm64/lib \
+	-L ~/homebrew/Cellar/sqlite/3.49.1/lib \
+	-I ~/homebrew/Cellar/sqlite/3.49.1/include \
+	-lwolfssl -lsqlite3 \
+	-o run_tests
 
 g++ -std=c++17 ./main.cpp xios.o \
-    -I build_x86_64/include \
-    -L build_x86_64/lib \
-    -I build-osx-arm64/wolfssl \
-    -L build-osx-arm64/lib \
-    -L ~/homebrew/Cellar/sqlite/3.49.1/lib \
-    -I ~/homebrew/Cellar/sqlite/3.49.1/include \
-    -lwolfssl -lsqlite3 \
-    -o xios_driver
+	-I build_x86_64/include \
+	-L build_x86_64/lib \
+	-I build-osx-arm64/wolfssl \
+	-L build-osx-arm64/lib \
+	-L ~/homebrew/Cellar/sqlite/3.49.1/lib \
+	-I ~/homebrew/Cellar/sqlite/3.49.1/include \
+	-lwolfssl -lsqlite3 \
+	-o xios_driver
 
-g++ -std=c++17 ./test/test_server/test_server.cpp xios.o \
-    -I build_x86_64/include \
-    -L build_x86_64/lib \
-    -I build-osx-arm64/wolfssl \
-    -L build-osx-arm64/lib \
-    -L ~/homebrew/Cellar/sqlite/3.49.1/lib \
-    -I ~/homebrew/Cellar/sqlite/3.49.1/include \
-    -lwolfssl -lsqlite3 \
-    -o test_server
 
 npm update
 npm i node-gyp
@@ -81,28 +71,25 @@ echo 'y' | npx node-gyp configure build
 source ./venv/bin/activate
 
 echo "Starting proxy server..."
-./util/reverse-https-proxy 127.0.0.1 8080 127.0.0.1 1443 > proxy.log 2>&1 &
+./util/reverse-https-proxy 127.0.0.1 8080 127.0.0.1 1443 >proxy.log 2>&1 &
 PROXY_PID=$!
-sleep 2  # Wait for proxy to start
+sleep 2 # Wait for proxy to start
 
-python test/test_rest_api/app.py > backend.log 2>&1 &
+python test/test_rest_api/app.py >backend.log 2>&1 &
 BACKEND_PID=$!
-sleep 2 
+sleep 2
 
 echo "Running test client..."
 if ./run_tests; then
-    echo "Integration test PASSED"
-    kill "$PROXY_PID"
-    kill "$BACKEND_PID"
-    exit 0
+	echo "Integration test PASSED"
+	kill "${PROXY_PID}"
+	kill "${BACKEND_PID}"
+	exit 0
 else
-    echo "Integration test FAILED"
-    cat proxy.log || true
-    cat backend.log || true
-    kill "$PROXY_PID"
-    kill "$BACKEND_PID"
-    exit 1
+	echo "Integration test FAILED"
+	cat proxy.log || true
+	cat backend.log || true
+	kill "${PROXY_PID}"
+	kill "${BACKEND_PID}"
+	exit 1
 fi
-
-
-
